c06106343cb140494a2fbb54fc48caab17e31a87
siraj9949 <shaiksirajuddin9949@gmail.com>
2023-12-23 17:23:16 +0530

    callbacks

--- helper_functions.py
+++ helper_functions.py
@@ -18,17 +18,6 @@ def unzip_data(filename):
     zip_ref.close()
 
 
-def earlyStop(pat, loss_num=0):
-    losses = ["val_loss", "val_accuracy", "val_mse", "val_mae"]
-    return tf.keras.callbacks.EarlyStopping(
-        monitor=losses[
-            loss_num
-        ],  # Metric to monitor for early stopping (e.g., 'val_loss', 'val_accuracy')
-        patience=pat,  # Number of epochs with no improvement after which training will be stopped
-        restore_best_weights=True,  # Restores model weights from the epoch with the best monitored value
-    )
-
-
 def kaggleCsv1(preds, columns, id_start=1, onehoted=False):
     if onehoted:
         preds = np.argmax(preds, axis=1)
@@ -52,3 +41,94 @@ def kaggleCsv(preds, columns, id_start=1, onehoted=False):
 
     submission_df = pd.DataFrame(d)
     submission_df.to_csv("submission1.csv", index=False)
+
+
+import matplotlib.pyplot as plt
+
+
+def plot_loss_curve(history):
+    """
+    Plots the training and validation loss curves from the given training history.
+
+    Parameters:
+    - history: A dictionary containing training history, typically obtained from model.fit().
+    """
+    plt.plot(history["loss"], label="Training Loss")
+    plt.plot(history["val_loss"], label="Validation Loss")
+    plt.title("Training and Validation Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.show()
+
+
+def callbacksV1(
+    name,
+    patience=3,
+    filepath="/model",
+    monitor="loss",
+    save_best_only=True,
+    save_weights_only=True,
+    min_lr=0.00001,
+    factor=0.2,
+):
+    if name == "earlystop":
+        # losses = ["val_loss", "val_accuracy", "val_mse", "val_mae"]
+        return tf.keras.callbacks.EarlyStopping(
+            monitor=monitor,  # Metric to monitor for improvement
+            min_delta=0.001,  # Minimum change in monitored metric to qualify as improvement
+            patience=patience,  # Number of epochs to wait for improvement before stopping
+            verbose=1,  # Verbosity mode (0 = silent, 1 = progress bar, 2 = one line per epoch)
+            mode="auto",  # Whether to monitor for min ('min'), max ('max'), or no specific direction ('auto')
+            baseline=None,  # Baseline value for the monitored metric to compare against
+            restore_best_weights=True,  # Whether to restore model weights from the epoch with the best monitored metric
+        )
+    elif name == "modelckpt":
+        return tf.keras.callbacks.ModelCheckpoint(
+            filepath=filepath,  # model_weights.h5
+            monitor=monitor,  # Metric to monitor for improvement
+            verbose=1,
+            save_best_only=save_best_only,  # Save only the model with the best monitored metric
+            save_weights_only=save_weights_only,  # Save only model weights, not entire model architecture
+            mode="min",  # Monitor for minimum monitored metric
+        )
+    elif name == "autoreducer":
+        return tf.keras.callbacks.ReduceLROnPlateau(
+            monitor="val_loss",  # Metric to monitor for plateau
+            factor=factor,  # Factor by which to reduce the learning rate
+            patience=patience,  # Number of epochs to wait before reducing the learning rate
+            min_lr=min_lr,  # Minimum learning rate to reach
+            verbose=1,  # Verbosity mode
+        )
+    elif name == "lrshedule":
+
+        def lr_schedule(epoch):
+            # Example schedule: decrease learning rate by a factor of 10 every 20 epochs
+            lr = factor * (0.1 ** (epoch // width))
+            return lr
+
+        return tf.keras.callbacks.LearningRateScheduler(lr_schedule)
+
+    else:
+        print("---plrrr")
+    return None
+
+
+def useTpu():
+    # Connect to the TPU
+    try:
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver()  # TPU detection
+        print("Running on TPU ", tpu.master())
+    except ValueError:
+        tpu = None
+
+    if tpu:
+        tf.config.experimental_connect_to_cluster(tpu)
+        tf.tpu.experimental.initialize_tpu_system(tpu)
+        strategy = tf.distribute.TPUStrategy(tpu)
+    else:
+        # Default strategy for CPU and single GPU
+        strategy = tf.distribute.get_strategy()
+
+    print("Number of devices: {}".format(strategy.num_replicas_in_sync))
+    return strategy
